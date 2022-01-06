6e3e61c6d63d396449a6c9bf6e3c3e74ca8dc763
Leandro <play3rtox1c@gmail.com>
2022-01-06 11:01:23 -0300

    Updates

---
+++ precache-manifest.c3b508f713102b4fdab6e7986deeb3a8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57501a91acd96d9689f2b9ab5f5ba529",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "02638655aee4497d6ff5",
+    "url": "/recipes-app/static/css/main.de32baf0.chunk.css"
+  },
+  {
+    "revision": "c7e30d2aa4c39892b3c4",
+    "url": "/recipes-app/static/js/2.88b169ed.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipes-app/static/js/2.88b169ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02638655aee4497d6ff5",
+    "url": "/recipes-app/static/js/main.1a322ddb.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "16197948d2b3f1f84febcab25be9e1e3",
+    "url": "/recipes-app/static/media/area-location.16197948.jpg"
+  },
+  {
+    "revision": "0a2607ce4b9da379252c84e945860462",
+    "url": "/recipes-app/static/media/background.0a2607ce.jpg"
+  },
+  {
+    "revision": "b0d54d928dc627b323eab5aa71ad33e4",
+    "url": "/recipes-app/static/media/drink-background.b0d54d92.jpg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "29c6b32d91910ac4e4c21cb683e52bec",
+    "url": "/recipes-app/static/media/food-background.29c6b32d.jpg"
+  },
+  {
+    "revision": "c43c85459f058d0d04fd39cf14b2ce86",
+    "url": "/recipes-app/static/media/food-ingredients.c43c8545.jpg"
+  },
+  {
+    "revision": "fb2c4c4c448aac6984313dfe88b74f57",
+    "url": "/recipes-app/static/media/imLucky-drink.fb2c4c4c.jpg"
+  },
+  {
+    "revision": "d62a69a4dc05d1b85f57cddcdc8894f2",
+    "url": "/recipes-app/static/media/imLucky-food.d62a69a4.jpg"
+  },
+  {
+    "revision": "c9726214cab2c77c79f71d5ed37c75d8",
+    "url": "/recipes-app/static/media/login-image.c9726214.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "717c73343ecc9d1625acf0c8ce3c5580",
+    "url": "/recipes-app/static/media/vodka.717c7334.jpg"
+  }
+]);
